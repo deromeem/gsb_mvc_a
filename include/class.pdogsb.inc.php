d4bc56d6487166bac41c2a609d22084593fa22c5
Princy BOANALIDY <princyboanalidy@gmail.com>
2021-11-15 11:31:30 +0100

    modifications test 1 BOANALIDY

--- include/class.pdogsb.inc.php
+++ include/class.pdogsb.inc.php
@@ -49,7 +49,8 @@ class PdoGsb{
  * @param $mdp
  * @return l'id, le nom et le prénom sous la forme d'un tableau associatif 
 */
-	public function getInfosVisiteur($login, $mdp){
+	public function getInfosVisiteur($login, $mdp){ 
+		// Méthode permettant de récupérer les infos d'un visiteur
 		// $req = "select visiteur.id as id, visiteur.nom as nom, visiteur.prenom as prenom from visiteur 
 		// where visiteur.login='$login' and visiteur.mdp='$mdp'";
 		// $rs = PdoGsb::$monPdo->query($req);
