424474c9bcea2b0aa128854505b494e45e2c0b08
Maxime FRAGNET <fragnet.max@gmail.com>
2021-11-15 10:50:13 +0100

    modification test 1 FRAGNET

--- include/class.pdogsb.inc.php
+++ include/class.pdogsb.inc.php
@@ -50,6 +50,7 @@ class PdoGsb{
  * @return l'id, le nom et le prénom sous la forme d'un tableau associatif 
 */
 	public function getInfosVisiteur($login, $mdp){
+		// ceci est un commentaire commenté par le commentateur de commentaire commenté.
 		// $req = "select visiteur.id as id, visiteur.nom as nom, visiteur.prenom as prenom from visiteur 
 		// where visiteur.login='$login' and visiteur.mdp='$mdp'";
 		// $rs = PdoGsb::$monPdo->query($req);
